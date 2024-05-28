c691e6f69dbae989358bec82526a1b9a056f7965
dg-delbank <165733779+dg-delbank@users.noreply.github.com>
2024-05-28 17:10:30 -0300

    a

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const app = require('./src/server');
+const os = require('os');
 
 const PORT = process.env.PORT || 3000;
 
+const networkInterfaces = os.networkInterfaces();
+let ipAddress = '';
+
+Object.keys(networkInterfaces).forEach((interfaceName) => {
+    networkInterfaces[interfaceName].forEach((iface) => {
+        if (iface.family === 'IPv4' && !iface.internal) {
+            ipAddress = iface.address;
+        }
+    });
+});
+
+if (!ipAddress) {
+    ipAddress = 'localhost';
+}
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at http://${ipAddress}:${PORT}`);
 });
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('API está funcionando!');
+  res.status(200).send('API está funcionando!');
 });
 
 module.exports = router;
